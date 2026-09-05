2b6dab90833c655c8f7f9547c82d255d86c79e9e
m3xadam <adamabdullahi559@gmail.com>
2026-09-05 13:57:02 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function gotoport(){
+    window.location.href="portfolio.html";
+}
+
+function gotocont(){
+    window.location.href='mailto:adamabdullahi559@gmail.com';
+}
+
+function gotowh(){
+    window.location.href='https://wa.me/message/2S2G3QAFAMEPB1';
+}
